b5de7d36835fc4e0c87dfdfe038bda3a439c8c7f
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-01-15 19:06:48 +0300

    download books from all pages
    download books from all pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,34 +6,125 @@ from urllib.error import HTTPError
 from urllib.parse import urljoin
 
 
-n = 0
+def check_for_redirect(book_page):
+    if book_page.history:
+        raise requests.exceptions.HTTPError
 
-#  https://tululu.org/l55/2/
-url = 'https://tululu.org/l55/'
-book_page = requests.get(url)
-book_page.raise_for_status()
-soup = BeautifulSoup(book_page.text, 'lxml')
-#book_short_link = soup.select_one('div.bookimage a').get('href')
-#full_link = urljoin(f'https://tululu.org/l55/', book_short_link)
-links = soup.find_all('div', class_='bookimage')
-for link in links:
-    n += 1
-    short_link = urljoin(f'https://tululu.org/l55/', link.find('a').attrs['href'])
-    print(n, short_link)
 
-for page in range (1, 11):
-    url = f'https://tululu.org/l55/{page}'
-    book_page = requests.get(url)
+def make_soup(book_link):
+
+    book_page = requests.get(book_link)
     book_page.raise_for_status()
     soup = BeautifulSoup(book_page.text, 'lxml')
+    check_for_redirect(book_page)
 
-    links = soup.find_all('div', class_='bookimage')
-    for link in links:
-        n += 1
-        short_link = urljoin(f'https://tululu.org/l55/', link.find('a').attrs['href'])
-        print(n, short_link)
+    return soup
 
 
+def get_book_link_credentials(soup, book_link):
+
+    book_credentials = []
+
+    book_title_author = soup.select_one('.ow_px_td h1').text
+    title, author = book_title_author.split(' \xa0 :: \xa0 ')
+
+    book_pic_link = soup.select_one('div.bookimage img')['src']
+    book_pic_link = urljoin(book_link, book_pic_link)
+
+    book_download_link = soup.select('table.d_book a')[8].get('href')
+    book_download_link = urljoin(book_link, book_download_link)
+
+    book_credentials.append(book_download_link)
+    book_credentials.append(title)
+    book_credentials.append(book_pic_link)
+    book_credentials.append(author)
+
+    return book_credentials
 
 
+def download_txt(url, filename, folder='books/'):
 
+    book_page = requests.get(url, allow_redirects=False)
+    book_page.raise_for_status()
+    check_for_redirect(book_page)
+    filename = sanitize_filename(filename)
+
+    with open(f"{os.path.join(folder, filename)}", 'wb') as file:
+        file.write(book_page.content)
+
+
+def download_book_cover(filename, book_cover_url, folder='images/'):
+
+    book_cover = requests.get(book_cover_url, allow_redirects=False)
+    book_cover.raise_for_status()
+    check_for_redirect(book_cover)
+    filename = sanitize_filename(filename)
+
+    with open(f"{os.path.join(folder, filename)}.jpg", 'wb') as file:
+        file.write(book_cover.content)
+
+
+def main():
+
+    folders = ['books', 'images']
+
+    for folder in folders:
+
+        parent_dir = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(parent_dir, folder)
+
+        try:
+            os.makedirs(path, exist_ok = True)
+            print("Folder '%s' created successfully" %folder)
+        except OSError as error:
+            print("Folder '%s' can not be created")
+
+    #parser = argparse.ArgumentParser(description="The script downloads books from tululu portal")
+    #parser.add_argument("-s", "--start_id", default=1, help="Starting book id", type=int)
+    #parser.add_argument("-e", "--end_id", default=2, help="Ending book id", type=int)
+    #args = parser.parse_args()
+
+    #start_id = args.start_id
+    #end_id = args.end_id
+    #end_id = end_id + 1
+
+    n = 0
+
+    for page in range (0, 6):
+        if not page:
+            url = f'https://tululu.org/l55'
+        else:
+            url = f'https://tululu.org/l55/{page}'
+
+        try:
+
+            book_page = requests.get(url)
+            book_page.raise_for_status()
+            soup = BeautifulSoup(book_page.text, 'lxml')
+            links = soup.find_all('div', class_='bookimage')
+
+            for link in links:
+                n += 1
+                book_link = urljoin(f'https://tululu.org/l55/', link.find('a').attrs['href'])
+                print(n, book_link, ' making soup')
+
+                soup2 = make_soup(book_link)
+                url, filename, book_cover_url, author = get_book_link_credentials(soup2, book_link)
+
+                download_txt(url, f'{n}_{filename}')
+                download_book_cover(f'{n}_{filename}', book_cover_url)
+
+        except requests.exceptions.ConnectionError:
+            logging.exception('Connection issues, will retry after timeout.')
+            print('Connection issues, will retry after timeout.')
+            time.sleep(30)
+        except requests.exceptions.HTTPError:
+            print('HTTP Error, broken link or redirect')
+        except TypeError:
+            print(f'Unable to make soup for book {n} due to insufficient data')
+        except IndexError:
+            print(f'Unable to download book {n} due to missing link')
+
+
+if __name__ == '__main__':
+    main()
